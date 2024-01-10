913b4386623d8a32fc2a85242cfefa6b2e34a742
Islam Askar <islam.askar@gmail.com>
2024-01-10 17:01:21 +0100

    feat: refactor the collector class to allow passing extra filtration if needed also allow populating and return lean documents

--- src/executor/collector.ts
+++ src/executor/collector.ts
@@ -10,6 +10,8 @@ export type QueryExecutor<T> = {
   exec(): Promise<T>;
   skip(offset: number): QueryExecutor<T>;
   limit(limit: number): QueryExecutor<T>;
+  lean(): any;
+  populate(relation: string[]);
   sort(data: SortableParameters): QueryExecutor<T>;
 };
 
@@ -21,10 +23,29 @@ export type Model = {
 export class DocumentCollector<T> {
   constructor(private model: Model) {}
 
-  async find(query: CollectionDto): Promise<CollectionResponse<T>> {
+  scopedFilter(
+    userFilter: FilterableParameters,
+    scope: FilterableParameters,
+  ): FilterableParameters {
+    if (scope && Object.keys(scope).length === 0) {
+      return userFilter;
+    }
+
+    // If we have a filter and scope, apply both
+    return {
+      $and: [userFilter, scope],
+    };
+  }
+  async find(
+    query: CollectionDto,
+    scope?: FilterableParameters,
+    populate?: string[],
+  ): Promise<CollectionResponse<T>> {
     const q = this.model
-      .find(query.filter)
-      .skip(query.page * query.limit)
+      .find(this.scopedFilter(query.filter, scope) ?? query.filter)
+      .populate(populate ?? [])
+      .lean()
+      .skip((query.page - 1) * query.limit)
       .limit(query.limit);
 
     if (query.sorter) {
@@ -38,25 +59,29 @@ export class DocumentCollector<T> {
     const data = (await q.exec()) as T[];
     return {
       data,
-      pagination: await this.paginate(query),
+      pagination: await this.paginate(query, scope),
     };
   }
 
-  private async paginate(query: CollectionDto) {
-    const count: number = await this.count(query);
+  private async paginate(query: CollectionDto, scope?: FilterableParameters) {
+    const count: number = await this.count(query, scope);
     const pagination: Pagination = {
       total: count,
       page: query.page,
       limit: query.limit,
-      next:
-        (query.page + 1) * query.limit >= count ? undefined : query.page + 1,
-      prev: query.page == 0 ? undefined : query.page - 1,
+      next: query.page * query.limit >= count ? undefined : query.page + 1,
+      prev: query.page == 1 ? undefined : query.page - 1,
     };
 
     return pagination;
   }
 
-  async count(query: CounterDto): Promise<number> {
-    return this.model.countDocuments(query.filter).exec();
+  async count(
+    query: CounterDto,
+    scope?: FilterableParameters,
+  ): Promise<number> {
+    return this.model
+      .countDocuments(this.scopedFilter(query.filter, scope) ?? query.filter)
+      .exec();
   }
 }
