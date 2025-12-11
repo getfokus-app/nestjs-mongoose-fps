d1ed5ffd7e64a670059b7656670b3044d72367bf
Islam Askar <islam.askar@creditshelf.com>
2025-12-11 12:26:24 +0100

    feat: Update version to 2.2.0 and enhance exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-export { CollectionDto } from './input.dto';
+export { CollectionDto, FilterableParameters, SortableParameters } from './input.dto';
 export { CollectionResponse, Pagination } from './output.dto';
 export { CollectionProperties, CollectionPropertyOptions } from './property';
 export { FilterValidationError } from './filter/validation.error';
 export { SortValidationError } from './sorter/validation.error';
-export { DocumentCollector } from './executor/collector';
+export {
+  DocumentCollector,
+  FindOptions,
+  PopulateOptions,
+} from './executor/collector';
 export { ValidationPipe } from './validation.pipe';
 export { Expose } from './property.decorator';
